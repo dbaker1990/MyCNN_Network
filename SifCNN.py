5ea76d748dd3fe0aa8dde09cd01a3a4583e73d52
dbaker1990 <dominicbaker16@gmail.com>
2023-08-09 14:00:40 -0400

    add

--- SifCNN.py
+++ SifCNN.py
@@ -1,6 +1,10 @@
 import torch
+import torchvision
+import torch.optim as optim
 from torchvision import transforms
+from torchvision.datasets import ImageFolder
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 import cv2 
 import matplotlib.pyplot as plt
@@ -8,3 +12,7 @@ import time
 
 haveCuda  = torch.cuda.is_available()
 print(torch.cuda.get_device_name(0))
+
+batch_size = 32
+n_epochs = 20
+learning_rate = 0.001
