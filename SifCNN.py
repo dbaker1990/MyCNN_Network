f0f2eaf558f5e0922b258c4ea1fbecac4589825e
dbaker1990 <dominicbaker16@gmail.com>
2023-08-09 15:42:19 -0400

    idk

--- SifCNN.py
+++ SifCNN.py
@@ -3,6 +3,7 @@ import torchvision
 import torch.optim as optim
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
+from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
@@ -15,8 +16,9 @@ haveCuda  = torch.cuda.is_available()
 print(torch.cuda.get_device_name(0))
 
 batch_size = 32
-n_epochs = 20
+n_epochs = 10
 learning_rate = 0.001
+shuffleImages = True
 
 #Set the training folder and set labels for each folder
 data = ImageFolder(root="./train")
