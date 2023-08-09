a9d6ac7cb2a7c092397552a8e8b46de5d42407df
dbaker1990 <dominicbaker16@gmail.com>
2023-08-09 14:10:11 -0400

    labels

--- SifCNN.py
+++ SifCNN.py
@@ -10,9 +10,14 @@ import cv2
 import matplotlib.pyplot as plt
 import time
 
+#Check to see if cuda is available if so show the name of the gpu
 haveCuda  = torch.cuda.is_available()
 print(torch.cuda.get_device_name(0))
 
 batch_size = 32
 n_epochs = 20
 learning_rate = 0.001
+
+#Set the training folder and set labels for each folder
+data = ImageFolder(root="./train")
+print(data.class_to_idx)
